6374587cd4365848cd34aca04d22808005a67aa0
Ciril P Thomas <52671249+Cirilplackal@users.noreply.github.com>
2024-06-11 21:05:19 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,3 @@
-
 def update_probe():
     num = int(input("Enter a number: "))
     file_path = r"C:\Users\ciril\OneDrive\Desktop\probe.txt"  # Updated file path
@@ -6,6 +5,20 @@ def update_probe():
         with open(file_path, 'r') as file:
             lines = file.readlines()
         new_line = '\nstop -create -time ' + str(num) + 'ns-absolute-execute "save mysnap_' + str(num) + 'n-overwrite" -continue\n'
+        lines.append(new_line)
+        with open(file_path, 'w') as file:
+            file.writelines(lines)
+        print("Updated probe")
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+
+def update_probe_again():
+    num = int(input("Enter the same number again: "))
+    file_path = r"C:\Users\ciril\OneDrive\Desktop\probe.txt"  # Updated file path
+    try:
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+        new_line = '\n-r mySnap_'+ str(num)+'n'
         lines.append(new_line)
         with open(file_path, 'w') as file:
             file.writelines(lines)
@@ -60,21 +73,26 @@ def main():
         print("1. Update Probe")
         print("2. Update xrunArgs")
         print("3. Replace Stop Time in Tran")
+        print("4. Update the probe again")
         print("0. Exit")
 
-        choice = input("Enter your choice (0/1/2/3): ")
+        choice = input("Enter your choice (0/1/2/3/4): ")
         if choice == '0':
             print("Exiting the program.")
             break
 
-        if choice in ['1', '2', '3']:
+        if choice in ['1', '2', '3','4']:
             if choice == '1':
                 update_probe()
             elif choice == '2':
                 update_xrunArgs()
             elif choice == '3':
                 replace_string_in_tran()
+            elif choice == '4':
+                update_probe_again()
         else:
             print("Invalid choice. Please enter a valid option.")
 
 main()
+
+
